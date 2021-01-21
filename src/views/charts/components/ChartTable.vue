cab00a392bff0154dddedc2ad87621dbea96e7ed
chuge <fantasysango@163.com>
2021-01-21 08:33:22 +0800

    commit

---
+++ src/views/charts/components/ChartTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <a-table
+    bordered
+    size="small"
+    :pagination="false"
+    :data-source="tableData"
+    :columns="tableCols"
+  ></a-table>
+    <!-- :scroll="{ x: true }" -->
+    <!-- showPagination="auto" -->
+</template>
+
+<script>
+export default {
+  name: 'ChartTable',
+  props: {
+    tableConf: {
+      type: Object,
+      default: () => ({ columns: [], rows: [] })
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    tableCols() {
+      let { columns } = this.tableConf
+      return columns.map(d => {
+        return typeof d === 'string' ? {
+          title: d,
+          dataIndex: d
+        }: d
+      })
+    },
+    tableData() {
+      let { columns, rows } = this.tableConf
+      return rows
+    }
+  },
+  watch: {},
+  created() {
+  },
+  methods: {
+  },
+}
+</script>
+<style lang="less" scoped>
+</style>
